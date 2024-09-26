648c12b1ba18a81cbee326200fa4fe82dada4967
AmitavG <amitavgodishala@gmail.com>
2024-09-26 09:32:49 +0530

    add dom methods to display the result

--- game.js
+++ game.js
@@ -30,40 +30,55 @@ let maxRounds = 5;
 function playRound(humanChoice, computerChoice){
     if(humanChoice == computerChoice){
         console.log(`It is a draw`);
+        document.getElementById("round").innerHTML=`Its a draw, lol`;
     }
-    else if(humanChoice=="rock"){
-        if(computerChoice=="scissors"){
-            humanScore++;
-            console.log(`You won this round! ${humanChoice} beats ${computerChoice}`);
-        }
-        else{
-            computerScore++;
-            console.log(`You lost this round! ${humanChoice} beats ${computerChoice}`);
-        }
+    // else if(humanChoice=="rock" ){
+    //     if(computerChoice=="scissors"){
+    //         humanScore++;
+    //         console.log(`You won this round! ${humanChoice} beats ${computerChoice}`);
+    //     }
+    //     else{
+    //         computerScore++;
+    //         console.log(`You lost this round! ${humanChoice} beats ${computerChoice}`);
+    //     }
+    // }
+    // else if(humanChoice=="paper"){
+    //     if(computerChoice=="rock"){
+    //         humanScore++;
+    //         console.log(`You won this round! ${humanChoice} beats ${computerChoice}`);
+    //     }
+    //     else{
+    //         computerScore++;
+    //         console.log(`You lost this round! ${humanChoice} beats ${computerChoice}`);
+    //     }
+    // }
+    // else if(humanChoice=="scissors"){
+    //     if(computerChoice=="paper"){
+    //         humanScore++;
+    //         console.log(`You won this round! ${humanChoice} beats ${computerChoice}`);
+    //     }
+    //     else{
+    //         computerScore++;
+    //         console.log(`You lost this round! ${humanChoice} beats ${computerChoice}`);
+    //     }
+    // }
+
+    else if((humanChoice=="rock" && computerChoice=="scissors") || (humanChoice=="paper" && computerChoice=="rock") || (humanChoice=="scissors" && computerChoice=="paper")){
+        humanScore++;
+        console.log("You won this round!");
+        document.getElementById("round").innerHTML=`You won this round!!!`
     }
-    else if(humanChoice=="paper"){
-        if(computerChoice=="rock"){
-            humanScore++;
-            console.log(`You won this round! ${humanChoice} beats ${computerChoice}`);
-        }
-        else{
-            computerScore++;
-            console.log(`You lost this round! ${humanChoice} beats ${computerChoice}`);
-        }
+
+    else{
+        computerScore++;
+        console.log("You (unfortunately) lost this round!");
+        document.getElementById("round").innerHTML=`You (unfortunately) lost this round`;
     }
-    else if(humanChoice=="scissors"){
-        if(computerChoice=="paper"){
-            humanScore++;
-            console.log(`You won this round! ${humanChoice} beats ${computerChoice}`);
-        }
-        else{
-            computerScore++;
-            console.log(`You lost this round! ${humanChoice} beats ${computerChoice}`);
-        }
-    }
+
 
     console.log(`Human Score: ${humanScore}, Computer Score: ${computerScore}`);
     console.log("\n");
+    document.getElementById("score").innerHTML=`Human score : ${humanScore} - ${computerScore} : Computer score`;
 }
 
 function playGame(humanChoice){
@@ -72,16 +87,26 @@ function playGame(humanChoice){
         console.log(`computer choice: ${computerSelection}`);
         console.log(`human choice: ${humanChoice}`);
 
+        document.getElementById("computer").innerHTML=`computer chose: ${computerSelection}`;
+        document.getElementById("human").innerHTML=`human chose: ${humanChoice}`;
+        
+
         playRound(humanChoice, computerSelection);
         maxScore = Math.max(humanScore, computerScore);
 
         if(maxScore == maxRounds){
-            if(humanScore > computerScore)
+            if(humanScore > computerScore){
+                document.getElementById("result").textContent="WE DEFEATED AI ( ͡° ͜ʖ ͡° )";
                 console.log(`You won the game with ${humanScore} - ${computerScore}`);
-            else if(humanScore < computerScore)
+            }
+            else if(humanScore < computerScore){
+                document.getElementById("result").textContent="AI DEFEATED US (⊙▂⊙✖ )";
                 console.log(`You lost the game (unfortunately) with ${humanScore} - ${computerScore}`);
-            else
+            }
+            else{
+                document.getElementById("result").textContent="Phew, its a draw, you can restart the game! ¯\_(ツ)_/¯";
                 console.log(`The game is tied with ${humanScore} - ${computerScore}`);
+            }
         }
     }
     else{
